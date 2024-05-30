b8ea2ff80fe07ae8e429d07071e73e5a037bc2a2
Subham Jena <subhamjena4519@gmail.com>
2024-05-30 17:13:38 +0530

    Complete UI Upload

---
+++ src/sections/Services.jsx
@@ -0,0 +1,13 @@
+import ServiceCard from "../component/ServiceCard"
+import {services} from "../constants/index"
+const Services = () => {
+  return (
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {services.map((service)=>(
+        <ServiceCard key={service.label} {...service}/>
+      ))}
+    </section>
+  )
+}
+
+export default Services
